aac1f9986719594c9bde169e164708b373f12ac7
hawkeye126 <pete.lower@gmail.com>
2014-12-24 14:09:54 -0800

    first commit, add plugin php that enqueues the js, add js file, add soundcloud svg file

---
+++ lowermedia-iframes-on-demand.js
@@ -0,0 +1,81 @@
+/*
+#
+#
+#
+#
+*/
+
+function vimeoLoadingThumb(id) {
+	var url = "http://vimeo.com/api/v2/video/" + id + ".json?callback=showThumb";
+	console.log(url);
+	var id_img = "#vimeo-" + id;
+	var script = document.createElement( 'script' );
+	script.type = 'text/javascript';
+	script.src = url;
+	jQuery(id_img).before(script);
+}
+
+function showThumb(data){
+	var id_img = "#vimeo-" + data[0].id;
+	jQuery(id_img).attr('src',data[0].thumbnail_large);
+}
+
+jQuery(document).ready(function(){
+	jQuery('body').addClass('iframes-ondemand');
+	var count = 0;
+
+	jQuery("iframe").each(function() {
+		count ++;
+		var src= jQuery(this).attr('src');
+		//fetch plugin url path
+		var pluginUrl = (iframeOnDemand.myurl);
+		//extract end of url
+		var shortSRC = src.substring(src.lastIndexOf('/') + 1);
+		var output = count+" "+src+" "+shortSRC;
+		var height, width;
+
+		//DETERMINE HEIGHT AND WIDTH
+		if(jQuery(this).attr('height')){
+			height= jQuery(this).attr('height');
+			output+=" "+height;
+		} else {
+			height="250";
+		}
+
+		//DETERMINE WIDTH
+		if(jQuery(this).attr('width')){
+			width= jQuery(this).attr('width');output+=" "+width;
+		} else {
+			width="250";
+		}
+		var svgPlay ="<svg  class='play-button-overlay-"+ count +"' style='margin:auto;left:"+width/1.23+"px;margin-top:"+height/2.57+"px;position:absolute;'><path fill-rule='evenodd' clip-rule='evenodd' fill='#1F1F1F' class='ytp-large-play-button-svg' d='M84.15,26.4v6.35c0,2.833-0.15,5.967-0.45,9.4c-0.133,1.7-0.267,3.117-0.4,4.25l-0.15,0.95c-0.167,0.767-0.367,1.517-0.6,2.25c-0.667,2.367-1.533,4.083-2.6,5.15c-1.367,1.4-2.967,2.383-4.8,2.95c-0.633,0.2-1.316,0.333-2.05,0.4c-0.767,0.1-1.3,0.167-1.6,0.2c-4.9,0.367-11.283,0.617-19.15,0.75c-2.434,0.034-4.883,0.067-7.35,0.1h-2.95C38.417,59.117,34.5,59.067,30.3,59c-8.433-0.167-14.05-0.383-16.85-0.65c-0.067-0.033-0.667-0.117-1.8-0.25c-0.9-0.133-1.683-0.283-2.35-0.45c-2.066-0.533-3.783-1.5-5.15-2.9c-1.033-1.067-1.9-2.783-2.6-5.15C1.317,48.867,1.133,48.117,1,47.35L0.8,46.4c-0.133-1.133-0.267-2.55-0.4-4.25C0.133,38.717,0,35.583,0,32.75V26.4c0-2.833,0.133-5.95,0.4-9.35l0.4-4.25c0.167-0.966,0.417-2.05,0.75-3.25c0.7-2.333,1.567-4.033,2.6-5.1c1.367-1.434,2.967-2.434,4.8-3c0.633-0.167,1.333-0.3,2.1-0.4c0.4-0.066,0.917-0.133,1.55-0.2c4.9-0.333,11.283-0.567,19.15-0.7C35.65,0.05,39.083,0,42.05,0L45,0.05c2.467,0,4.933,0.034,7.4,0.1c7.833,0.133,14.2,0.367,19.1,0.7c0.3,0.033,0.833,0.1,1.6,0.2c0.733,0.1,1.417,0.233,2.05,0.4c1.833,0.566,3.434,1.566,4.8,3c1.066,1.066,1.933,2.767,2.6,5.1c0.367,1.2,0.617,2.284,0.75,3.25l0.4,4.25C84,20.45,84.15,23.567,84.15,26.4z M33.3,41.4L56,29.6L33.3,17.75V41.4z'></path><polygon fill-rule='evenodd' clip-rule='evenodd' fill='#FFFFFF' points='33.3,41.4 33.3,17.75 56,29.6'></polygon></svg>";
+		var placeholderOverlay = "<div class='play-button-overlay-"+ count +"' style='margin:auto;left:"+width/1.26+"px;margin-top:"+height/3.214+"px;position:absolute;width:100px;height:100px;background-image:url("+pluginUrl+"/iframe-on-demand-play-overlay.svg);background-size:cover;'></div>";
+		var placeholderImg;
+
+		if (src.indexOf("youtube")!=-1) {
+			placeholderImg = "<img id='iframe-"+ count +"' class='iframe-youtube iframe-ondemand-placeholderImg iframe-"+ count +"' src='http://img.youtube.com/vi/"+ shortSRC +"/0.jpg' height="+ height +" width="+ width +" />";
+		} else if (src.indexOf("vimeo")!=-1) {
+			placeholderImg = "<img id='vimeo-"+ shortSRC +"' class='iframe-vimeo iframe-ondemand-placeholderImg iframe-"+ count +"' src='' height="+ height +" width="+ width +" />";
+		} else if (src.indexOf("soundcloud")!=-1) {
+			placeholderImg = "<img id='iframe-"+ count +"' class='iframe-soundcloud iframe-ondemand-placeholderImg iframe-"+ count +"' src='"+pluginUrl+"iframe-on-demand-play-soundcloud.svg' height="+ height +" width="+ width +" />";
+		} else if (src.indexOf("dailymotion")!=-1) {
+			placeholderImg = "<img id='iframe-"+ count +"' class='iframe-dailymotion iframe-ondemand-placeholderImg iframe-"+ count +"' src='http://www.dailymotion.com/thumbnail/video/"+ shortSRC +"' height="+ height +" width="+ width +" />";
+		} else {
+			placeholderImg = "<img id='iframe-"+ count +"' class='iframe-undetermined iframe-ondemand-placeholderImg iframe-"+ count +"' height="+ height +" width="+ width +" />";
+		}
+
+		//CREATE IFRAME VIDEO TO BE SHOWN AFTER PLACEHOLDER IMAGE IS CLICKED
+		var video = '<iframe width="'+ jQuery(this).attr('width') +'" height="'+ jQuery(this).attr('height') +'" border="2" src="'+ src +'"></iframe>';
+
+		//REPLACE IFRAME WITH IMAGE PLACEHOLDER
+		jQuery(this).replaceWith("<div class='iframe-wrap-"+count+"' style='height:"+height+"px;width:"+width+"px;'>"+svgPlay+placeholderImg+"</div>");
+
+		//LOAD IFRAME PLACEHOLDER FOR VIMEO IF NECESSARY
+		if (src.indexOf("vimeo")!=-1) {vimeoLoadingThumb(shortSRC);}
+
+		jQuery('.iframe-wrap-'+count).click(function(){
+			jQuery(".play-button-overlay-"+ count).remove();
+			jQuery(this).replaceWith(video);
+		});
+	});
+});
---
+++ lowermedia-iframes-on-demand.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: LowerMedia iFrames On Demand
+ * Plugin URI: http://
+ * Description: Reduce requests and optimize for speed!!! The iFrames On Demand plugin replaces all iframes on the page with an image placeholder, when the image placeholder is clicked the image appears.  This works without any configuration for all iframes and will pull in video thumbnails for YouTube, Vimeo and DailyMotion.
+ * Version: 0.0.1
+ * Author: Pete Lower
+ * Author URI: http://petelower.com
+ * Network: False
+ * License: GPL2
+ */
+ /*  Copyright 2014 Pete Lower (email : pete@petelower.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */ 
+/*############################################################################################
+#
+#   SECURITY: BLOCK DIRECT ACCESS TO FILE
+#
+*/
+
+	defined('ABSPATH') or die("Cannot access pages directly.");
+
+/*############################################################################################
+#
+#	ENQUEUE AND LOCALIZE
+#   Enqueue our 'iFrame On Demand' script and localize the plugin path for local asset use
+#
+*/
+	function lowermedia_iframe_ondemand()  
+	{  
+		wp_register_script( 'iframe-ondemand', plugins_url( '/lowermedia-iframes-on-demand.js' , __FILE__ ), array( 'jquery' ), '1.0.0', true);
+		wp_enqueue_script( 'iframe-ondemand' );
+		wp_localize_script('iframe-ondemand', 'iframeOnDemand', array('myurl' => plugins_url( '/' , __FILE__ )));
+	}  
+	add_action( 'wp_enqueue_scripts', 'lowermedia_iframe_ondemand' ); 
